d9b32abf37446fbfc92f96f735ce8db1b83572b7
Anshika2007 <developer1@omlogistics.co.in>
2020-09-22 17:50:50 +0530

    Added Google Key

--- routes/server.js
+++ routes/server.js
@@ -7,6 +7,7 @@ var axios = require('axios');
 app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
+var GOOGLE_KEY='ENTER YOUR KEY'
 
 router.get('/', function (req, res, next) {
     res.render('index');
@@ -15,17 +16,15 @@ router.get('/', function (req, res, next) {
 router.post('/data', function (req, res, next) {
 
     const loc = req.body.lat + ',' + req.body.lon;
-    console.log(loc);
-    // res.status(200).send("connected");
+
     try {
 
 
-        // axios.post(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=${loc}&key=AIzaSyA_9vXmXuR50az31l3oJnqqU9B9j0Kh90k`
-        axios.post(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${loc}&radius=50000&type=restaurant&key=AIzaSyA_9vXmXuR50az31l3oJnqqU9B9j0Kh90k`
+        axios.post(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${loc}&radius=50000&type=restaurant&key=${GOOGLE_KEY}`
 
 
         ).then(resp => {
-            // console.log(resp.data);
+         
             res.status(200).send(resp.data);
         });
     } catch (err) {
